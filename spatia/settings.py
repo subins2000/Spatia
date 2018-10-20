35b3e48912d3ff5da59d21dd594fdf72e5d07285
Subin Siby <subins2000@gmail.com>
2018-10-21 03:08:49 +0530

    set default order backend

--- spatia/settings.py
+++ spatia/settings.py
@@ -61,7 +61,10 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination' +
     '.LimitOffsetPagination',
-    'PAGE_SIZE': 20
+    'PAGE_SIZE': 20,
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
 }
 
 CORS_ORIGIN_ALLOW_ALL = False
